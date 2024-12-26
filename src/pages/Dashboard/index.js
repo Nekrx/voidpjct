174f5d3683a11a694197858695bfc1ff1daf42d2
nekrx <necroinktt@gmail.com>
2024-12-26 14:43:35 -0300

    Fiz mais algumas alterações no dashboard, porém descobri que tenho que alterar o título e possivelmente algo no css geral do app para quando clicar no botão de esconder a barra ele ajustar o tamanho da tela para a barra escondida

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,14 +7,149 @@ import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { collection, getDocs, orderBy, limit, startAfter, query } from "firebase/firestore";
 import { db } from "../../services/firebaseConnections";
-import {format} from "date-fns";
+import { format } from "date-fns";
 
-function App() {
+const listRef = collection(db, "Chamados");
+
+export default function Dashboard() {
+  const { logout } = useContext(AuthContext);
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [lastDocs, setLastDocs] = useState();
+  const [loadingMore, setLoadingMore] = useState(false);
+
+  async function fetchChamados() {
+    const q = query(listRef, orderBy("created", "desc"), limit(5));
+    const querySnapshot = await getDocs(q);
+
+    const isCollectionEmpty = querySnapshot.size === 0;
+    if (!isCollectionEmpty) {
+      let lista = [];
+      querySnapshot.forEach((doc) => {
+        lista.push(doc.data());
+      });
+
+      const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+      setChamados(lista);
+      setLastDocs(lastDoc);
+    } else {
+      setIsEmpty(true);
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchChamados();
+  }, []);
+
+  async function handleMore() {
+    setLoadingMore(true);
+
+    const q = query(listRef, orderBy("created", "desc"), startAfter(lastDocs), limit(5));
+    const querySnapshot = await getDocs(q);
+    const isCollectionEmpty = querySnapshot.size === 0;
+
+    if (!isCollectionEmpty) {
+      let lista = [];
+      querySnapshot.forEach((doc) => {
+        lista.push(doc.data());
+      });
+
+      const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+      setChamados((prevChamados) => [...prevChamados, ...lista]);
+      setLastDocs(lastDoc);
+    } else {
+      setIsEmpty(true);
+    }
+
+    setLoadingMore(false);
+  }
+
+  if (loading) {
     return (
-      <Header>
-        <Title></Title>
-      </Header>
+      <div>
+        <Header />
+        <div className="content">
+          <Title name="Dashboard">
+            <FiMessageSquare size={25} />
+          </Title>
+
+          <div className="container dashboard">
+            <span>Carregando...</span>
+          </div>
+        </div>
+      </div>
     );
   }
-  
-  export default App;
+
+  return (
+    <div>
+      <Header />
+      <div className="content">
+        <Title name="Dashboard">
+          <FiMessageSquare size={25} />
+        </Title>
+
+        {chamados.length === 0 ? (
+          <div className="container dashboard">
+            <span> Nenhum torneio encontrado...</span>
+          </div>
+        ) : (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Torneio</th>
+                  <th scope="col">Rodadas</th>
+                  <th scope="col">Timer</th>
+                  <th scope="col">Data</th>
+                </tr>
+              </thead>
+              {/* <tbody>
+                {chamados.map((item, index) => (
+                  <tr key={index}>
+                    <td data-label="Cliente">{item.cliente}</td>
+                    <td data-label="Assunto">{item.assunto}</td>
+                    <td data-label="Status">
+                      <span
+                        className="badge"
+                        style={{
+                          backgroundColor:
+                            item.status === "Aberto"
+                              ? "#5cb85c"
+                              : item.status === "Progresso"
+                              ? "#0275d8"
+                              : "#999",
+                        }}
+                      >
+                        {item.status}
+                      </span>
+                    </td>
+                    <td data-label="Cadastrado">{item.createdFormat}</td>
+                    <td data-label="#">
+                      <button className="action" style={{ backgroundColor: "#3583f6" }}>
+                        <FiSearch color="#FFF" size={17} />
+                      </button>
+                      <Link to={`/new/${item.id}`} className="action" style={{ backgroundColor: "#f6a935" }}>
+                        <FiEdit2 color="#FFF" size={17} />
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody> */}
+            </table>
+
+            {loadingMore && <h3>Buscando mais torneios...</h3>}
+            {!loadingMore && !isEmpty && (
+              <button className="btn-more" onClick={handleMore}>
+               Buscar mais...
+              </button>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
